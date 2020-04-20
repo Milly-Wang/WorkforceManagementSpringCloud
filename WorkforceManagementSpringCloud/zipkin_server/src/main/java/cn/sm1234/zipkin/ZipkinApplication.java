c7134b5b5aea85110ed23c7ea9bbcbf61f67b704
Milly-Wang <53628844+Milly-Wang@users.noreply.github.com>
2020-04-19 23:53:36 -0400

    Add files via upload

---
+++ WorkforceManagementSpringCloud/zipkin_server/src/main/java/cn/sm1234/zipkin/ZipkinApplication.java
@@ -0,0 +1,18 @@
+package cn.sm1234.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin.server.internal.EnableZipkinServer;
+
+/**
+ * Zipkin服务
+ */
+@SpringBootApplication
+@EnableZipkinServer
+public class ZipkinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinApplication.class,args);
+    }
+
+}
